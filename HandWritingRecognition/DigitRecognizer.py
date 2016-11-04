c804309c0c7f07f0a3b985c8654c5ab41a7f4d05
wanglaiqi <1191580068@qq.com>
2016-11-04 22:09:58 +0800

    revise the code structure

--- HandWritingRecognition/DigitRecognizer.py
+++ HandWritingRecognition/DigitRecognizer.py
@@ -1,9 +1,20 @@
-import numpy as np
+#import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-%matplotlib inline
+import csv
+#import matplotlib.pyplot as plt
+#%matplotlib inline
 from sklearn.preprocessing import StandardScaler
 
+
+#save the predict result into csv file
+def savePredictResult(image_y_predict,csvFielName):
+    with open(csvFielName,'wb') as myFile:
+        myWriter = csv.writer(myFile)
+        for i in image_y_predict:
+            tmp = []
+            tmp.append(i)
+            myWriter.writerow(tmp)
+            
 #algorithm1: use the knn classifier
 from sklearn.neighbors import KNeighborsClassifier
 def knnClassifier(image_X_train,image_y_train,image_X_test):
@@ -12,7 +23,7 @@ def knnClassifier(image_X_train,image_y_train,image_X_test):
     print(knn_clf)
     #make prediction
     image_y_test = knn_clf.predict(image_X_test)
-
+    savePredictResult(image_y_test,'SklearnKnnResult.csv')
 	
 #the main program about the program
 def HandingWritingRecognition():
@@ -29,13 +40,4 @@ def HandingWritingRecognition():
     image_test_std_X = stdsc.transform(image_test_X)
     #algorithm1: knn classifier
     knnClassifier(image_train_std_X,image_train_y,image_test_std_X)
-    #algorithm2: Lg classifier	
-	
-#save the predict result into csv file
-def savePredictResult(image_y_predict,csvFielName):
-    with open(csvFielName,'wb') as myFile:
-        myWriter = csv.writer(myFile)
-        for i in image_y_predict:
-            tmp = []
-            tmp.append(i)
-            myWriter.writerow(tmp)
+    #algorithm2: Lg classifier
